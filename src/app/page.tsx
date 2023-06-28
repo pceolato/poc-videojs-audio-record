b3c6267cb35e0f8ee33c8f09762bd78b5a61bdf1
Pedro Ceolato <pedroceolato2002@gmail.com>
2023-06-28 09:13:55 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,7 @@
-'use client'
-import * as React from "react";
-import { render } from "react-dom";
-import useRecorder from "./components/useRecorder";
-
 export default function Home() {
-  let [audioURL, isRecording, startRecording, stopRecording] = useRecorder() as any;
   return (
-    <div className="App">
-      <audio src={audioURL} controls />
-      <button onClick={startRecording} disabled={isRecording}>
-        start recording
-      </button>
-      <button onClick={stopRecording} disabled={!isRecording}>
-        stop recording
-      </button>
-
-      <p>
-        <em>
-          (On Codesandbox pop out the preview into a window to get a user media
-          request.)
-        </em>
-      </p>
+    <div>
+      App Videojs - Audio Record
     </div>
   );
 }
