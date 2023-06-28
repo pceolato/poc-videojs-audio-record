833265d86954a5ab6543d261009bb015e6db2d01
Pedro Ceolato <pedroceolato2002@gmail.com>
2023-06-28 09:38:34 -0300

    Update test

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -6,14 +6,21 @@ import useRecorder from "../components/useRecorder";
 export default function Test() {
   let [audioURL, isRecording, startRecording, stopRecording] = useRecorder() as any;
   return (
-    <div>
+    <div className="App">
       <audio src={audioURL} controls />
-      <button onClick={startRecording} disabled={isRecording} style={{ marginRight: 1rem }}>
+      <button onClick={startRecording} disabled={isRecording}>
         start recording
       </button>
       <button onClick={stopRecording} disabled={!isRecording}>
         stop recording
       </button>
+
+      <p>
+        <em>
+          (On Codesandbox pop out the preview into a window to get a user media
+          request.)
+        </em>
+      </p>
     </div>
   );
 }
