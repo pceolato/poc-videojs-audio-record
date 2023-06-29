723a6c9fba87777359d2e355d554bd0e581a8c08
Pedro <pedroceolato2002@gmail.com>
2023-06-29 19:41:43 -0300

    feat: added audio test

---
+++ src/app/audio/page.tsx
@@ -0,0 +1,7 @@
+export default function Audio() {
+    return (
+        <div className="h-screen flex justify-center items-center">
+            <audio src="https://cothefato-media.s3.amazonaws.com/file_1687998266880.webm" controls />
+        </div>
+    )
+}
